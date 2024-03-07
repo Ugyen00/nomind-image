bb80bb8895c75a0305ebc3dbdd1d615bf46e92c8
Sanam_App <ugyendechen00@gmail.com>
2024-03-07 18:28:34 +0600

    chag2

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -21,7 +21,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "nomind image",
+      dbName: "nomind-image.ai",
       bufferCommands: false,
     });
 
